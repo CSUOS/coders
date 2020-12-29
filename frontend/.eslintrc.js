8c5d16f258bdf5929acdc06d7cc5f9f67d760faa
leegwae <leegwae@gmail.com>
2020-12-30 05:35:08 +0900

    Add rules and Resolve error

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,21 +1,38 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-  },
-  extends: [
-    'airbnb', 'plugin:prettier/recommended',
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
+	parser: 'babel-eslint',
+	env: {
+		browser: true,
+		commonjs: true,
+		es2021: true,
+	},
+	extends: ['airbnb', 'prettier'],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+	},
+	plugins: ['prettier'],
+	rules: {
+		indent: ['error', 'tab'],
+		'react/jsx-indent': ['error', 'tab'],
+		'react/jsx-filename-extension': [
+			'error',
+			{
+				extensions: ['.js', '.jsx'],
+			},
+		],
+		'no-unused-vars': 'warn',
+		'no-console': 'off',
+		'prettier/prettier': [
+			'error',
+			{
+				tabWidth: 4,
+				useTabs: true,
+				endOfLine: 'auto',
+				singleQuote: true,
+				arrowParens: 'always',
+			},
+		],
+	},
 };
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,23 +4,23 @@ import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import { Login, Main } from './components/View';
 
 const theme = createMuiTheme({
-  typography: {
-    fontFamily: 'Noto Sans KR',
-  },
+	typography: {
+		fontFamily: 'Noto Sans KR',
+	},
 });
 
 function App() {
-  return (
-    <MuiThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Switch>
-          {/* <Route exact path="/" component={Login} /> */}
-          {/* <Route path="/home" component={Main} /> */}
-          <Route path="/" component={Main} />
-        </Switch>
-      </BrowserRouter>
-    </MuiThemeProvider>
-  );
+	return (
+		<MuiThemeProvider theme={theme}>
+			<BrowserRouter>
+				<Switch>
+					{/* <Route exact path="/" component={Login} /> */}
+					{/* <Route path="/home" component={Main} /> */}
+					<Route path="/" component={Main} />
+				</Switch>
+			</BrowserRouter>
+		</MuiThemeProvider>
+	);
 }
 
 export default App;

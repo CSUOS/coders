8f4c70df58c6b36d6aef5102fb3637022f13b251
leegwae <leegwae@gmail.com>
2020-12-30 09:35:07 +0900

    Add dropdown

--- frontend/src/components/UI/index.js
+++ frontend/src/components/UI/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from './Header';
 export { default as PageHeader } from './PageHeader';
 export { default as Dropdown } from './Dropdown';
+export { default as SearchInput } from './SearchInput';

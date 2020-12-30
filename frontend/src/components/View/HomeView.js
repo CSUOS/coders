12e369ca9f431951dbfd5794e95fa5d3ec60a997
leegwae <leegwae@gmail.com>
2020-12-30 09:51:33 +0900

    Add home view

--- frontend/src/components/View/HomeView.js
+++ frontend/src/components/View/HomeView.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+import { PageHeader, Dropdown, SearchInput, ProblemTable } from '../UI';
 
 const HomeView = () => {
+	const mainTitle = '문제 선택';
+	const label1 = '해결 여부';
+	const values = ['해결', '미해결'];
+	const label2 = '제목, 내용, 분류로 검색';
 	return (
 		<Grid className="home">
-			<Grid className="home-container">홈</Grid>
+			<Grid className="home-container">
+				<PageHeader mainTitle={mainTitle} />
+				<Grid className="home-content">
+					<Grid className="home-tableselector">
+						<SearchInput label={label2} />
+						<Grid className="home-dropdowns">
+							<Grid className="home-dropdown">
+								<Dropdown label={label1} values={values} />
+							</Grid>
+							<Grid className="home-dropdown">
+								<Dropdown label={label1} values={values} />
+							</Grid>
+							<Grid className="home-dropdown">
+								<Dropdown label={label1} values={values} />
+							</Grid>
+						</Grid>
+					</Grid>
+					<ProblemTable />
+				</Grid>
+			</Grid>
 		</Grid>
 	);
 };

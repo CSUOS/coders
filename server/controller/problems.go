15bef0d9f1cea97a29ea8db53e277f132f2d4fcb
unknown <heedoo0303@naver.com>
2021-01-04 03:57:37 +0900

    problem

---
+++ server/controller/problems.go
@@ -0,0 +1,174 @@
+package controller
+
+import (
+	"coders/httputil"
+	"coders/model"
+
+	"gorm.io/gorm"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+// ListProblems godoc
+// @Summary List Problems
+// @Description get Problems
+// @Tags Problems
+// @Accept  json
+// @Produce  json
+// @Success 200 {array} model.Problem
+// @Failure 400 {object} httputil.HTTPError
+// @Failure 404 {object} httputil.HTTPError
+// @Failure 500 {object} httputil.HTTPError
+// @Router /problems [get]
+func ListProblems(ctx *gin.Context) {
+	db := ctx.MustGet("db").(*gorm.DB)
+	problems, err := model.ProblemsAll(db)
+	if err != nil {
+		httputil.Error(ctx, http.StatusNotFound, err)
+		return
+	}
+	ctx.JSON(http.StatusOK, problems)
+}
+
+// ShowProblem godoc
+// @Summary Show an Problem
+// @Description get string by ID
+// @Tags Problems
+// @Accept  json
+// @Produce  json
+// @Param id path int true "Problem ID"
+// @Success 200 {object} model.Problem
+// @Failure 400 {object} httputil.HTTPError
+// @Failure 404 {object} httputil.HTTPError
+// @Failure 500 {object} httputil.HTTPError
+// @Router /problems/{id} [get]
+func ShowProblem(ctx *gin.Context) {
+	db := ctx.MustGet("db").(*gorm.DB)
+	id := ctx.Param("id")
+	aid, err := strconv.Atoi(id)
+	if err != nil {
+		httputil.Error(ctx, http.StatusBadRequest, err)
+		return
+	}
+	problem, err := model.ProblemOne(db, aid)
+	if err != nil {
+		httputil.Error(ctx, http.StatusNotFound, err)
+		return
+	}
+	ctx.JSON(http.StatusOK, problem)
+}
+
+// AddProblem godoc
+// @Summary Add an Problem
+// @Description add by json Problem
+// @Tags Problems
+// @Accept  json
+// @Produce  json
+// @Param Problem body model.EditProblem true "Add Problem"
+// @Success 200 {object} model.Problem
+// @Failure 400 {object} httputil.HTTPError
+// @Failure 404 {object} httputil.HTTPError
+// @Failure 500 {object} httputil.HTTPError
+// @Router /problems [post]
+func AddProblem(ctx *gin.Context) {
+	db := ctx.MustGet("db").(*gorm.DB)
+
+	var req model.EditProblem
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		httputil.Error(ctx, http.StatusBadRequest, err)
+		return
+	}
+	if err := req.ProblemValidation(); err != nil {
+		httputil.Error(ctx, http.StatusBadRequest, err)
+		return
+	}
+
+	problem := model.Problem{
+		Title: req.Title,
+		Desc: req.Desc,
+		TimeLimit: req.TimeLimit,
+		MemoryLimit: req.MemoryLimit,
+		ShortCircuit: req.ShortCircuit,
+	}
+	result, err := model.ProblemInsert(db, problem)
+	if err != nil {
+		httputil.Error(ctx, http.StatusBadRequest, err)
+		return
+	}
+	ctx.JSON(http.StatusOK, result)
+}
+
+
+// UpdateProblem godoc
+// @Summary Update an Problem
+// @Description Update by json Problem
+// @Tags Problems
+// @Accept  json
+// @Produce  json
+// @Param  id path int true "Problem ID"
+// @Param  Problem body model.EditProblem true "Update Problem"
+// @Success 200 {object} model.Problem
+// @Failure 400 {object} httputil.HTTPError
+// @Failure 404 {object} httputil.HTTPError
+// @Failure 500 {object} httputil.HTTPError
+// @Router /problems/{id} [patch]
+func UpdateProblem(ctx *gin.Context) {
+	db := ctx.MustGet("db").(*gorm.DB)
+	id := ctx.Param("id")
+	aid, err := strconv.Atoi(id)
+	if err != nil {
+		httputil.Error(ctx, http.StatusBadRequest, err)
+		return
+	}
+
+	var req model.EditProblem
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		httputil.Error(ctx, http.StatusBadRequest, err)
+		return
+	}
+
+	problem := model.Problem{
+		ID:   aid,
+		Title: req.Title,
+		Desc: req.Desc,
+		TimeLimit: req.TimeLimit,
+		MemoryLimit: req.MemoryLimit,
+		ShortCircuit: req.ShortCircuit,
+	}
+	result, err := model.ProblemUpdate(db, problem)
+	if err != nil {
+		httputil.Error(ctx, http.StatusNotFound, err)
+		return
+	}
+	ctx.JSON(http.StatusOK, result)
+}
+
+// DeleteProblem godoc
+// @Summary Delete an Problem
+// @Description Delete by Problem ID
+// @Tags Problems
+// @Accept  json
+// @Produce  json
+// @Param  id path int true "Problem ID" Format(int64)
+// @Success 204 {object} model.Problem
+// @Failure 400 {object} httputil.HTTPError
+// @Failure 404 {object} httputil.HTTPError
+// @Failure 500 {object} httputil.HTTPError
+// @Router /problems/{id} [delete]
+func DeleteProblem(ctx *gin.Context) {
+	db := ctx.MustGet("db").(*gorm.DB)
+	id := ctx.Param("id")
+	aid, err := strconv.Atoi(id)
+	if err != nil {
+		httputil.Error(ctx, http.StatusBadRequest, err)
+		return
+	}
+	err = model.ProblemDelete(db, aid)
+	if err != nil {
+		httputil.Error(ctx, http.StatusNotFound, err)
+		return
+	}
+	ctx.JSON(http.StatusNoContent, gin.H{})
+}
---
+++ server/model/problem.go
@@ -0,0 +1,87 @@
+package model
+
+import (
+	"errors"
+	"gorm.io/gorm"
+)
+
+type Problem struct {
+	ID   int    `json:"id" example:"1" format:"int64" gorm:"autoIncrement"`
+	Title string `json:"title" example:"Problem title"`
+	Desc string `json:"desc" example:"Problem description"`
+	TimeLimit int `json:"timeLimit" example:"1000" format:"int64"`
+	MemoryLimit int `json:"memoryLimit" example:"1024" format:"int64"`
+	ShortCircuit bool `json:"shortCircuit" example:"false"`
+}
+
+type EditProblem struct {
+	Title string `json:"title" example:"Problem title"`
+	Desc string `json:"desc" example:"Problem description"`
+	TimeLimit int `json:"timeLimit" example:"1000" format:"int64"`
+	MemoryLimit int `json:"memoryLimit" example:"1024" format:"int64"`
+	ShortCircuit bool `json:"shortCircuit" example:"false"`
+}
+
+var (
+	ErrTitleInvalid = errors.New("title is empty")
+	ErrDescInvalid = errors.New("description is empty")
+	ErrTimeLimitInvalid = errors.New("time limit is empty")
+	ErrMemoryLimitInvalid = errors.New("memory limit is empty")
+)
+
+func (p EditProblem) ProblemValidation() error {
+	switch {
+	case len(p.Title) == 0:
+		return ErrTitleInvalid
+	case len(p.Desc) == 0:
+		return ErrDescInvalid
+	case p.TimeLimit == 0:
+		return ErrTimeLimitInvalid
+	case p.MemoryLimit == 0:
+		return ErrMemoryLimitInvalid
+	default:
+		return nil
+	}
+}
+
+func ProblemsAll(db *gorm.DB) ([]Problem, error) {
+	var problems []Problem
+	err := db.Find(&problems).Error
+	return problems, err
+}
+
+func ProblemOne(db *gorm.DB, id int) ([]Problem, error) {
+	var problems []Problem
+	err := db.Where("id = ?", id).First(&problems).Error
+	return problems, err
+}
+
+func ProblemInsert(db *gorm.DB, problem Problem) (Problem, error) {
+	err := db.Create(&problem).Error
+	return problem, err
+}
+
+func ProblemUpdate(db *gorm.DB, problem Problem) (Problem, error) {
+	err := db.Model(&Problem{}).Where("id = ?", problem.ID).Update("title", problem.Title).Error
+	if err!=nil { return problem, err }
+	err = db.Model(&Problem{}).Where("id = ?", problem.ID).Update("desc", problem.Desc).Error
+	if err!=nil { return problem, err }
+	err = db.Model(&Problem{}).Where("id = ?", problem.ID).Update("time_limit", problem.TimeLimit).Error
+	if err!=nil { return problem, err }
+	err = db.Model(&Problem{}).Where("id = ?", problem.ID).Update("memory_limit", problem.MemoryLimit).Error
+	if err!=nil { return problem, err }
+	err = db.Model(&Problem{}).Where("id = ?", problem.ID).Update("short_circuit", problem.ShortCircuit).Error
+	if err!=nil { return problem, err }
+	return problem, err
+	// time_limit > timeLimit ?
+}
+
+func ProblemDelete(db *gorm.DB, id int) error {
+	var problem Problem
+	err := db.Where("id=?", id).First(&problem).Error
+	if err != nil{
+		return err
+	}
+	err = db.Delete(&problem).Error
+	return err
+}

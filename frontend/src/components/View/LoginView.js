1732fd40e41d3594bd44dda5e31abde327d3ad66
pySoo <55135881+pySoo@users.noreply.github.com>
2021-01-03 21:45:46 +0900

    after revert

--- frontend/src/components/View/LoginView.js
+++ frontend/src/components/View/LoginView.js
@@ -1,44 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Grid, TextField, Button, Box } from '@material-ui/core';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import LockIcon from '@material-ui/icons/Lock';
-import { LoginHeader } from '../UI';
 
 const LoginView = () => {
-	return (
-		<Grid className="login">
-			<LoginHeader />
-			<Box
-				borderColor="primary.main"
-				borderRadius="5%"
-				className="login-box"
-			>
-				<Grid container direction="row" className="login-container">
-					<Grid className="login-text">로그인</Grid>
-					<Grid container className="login-field">
-						<AccountCircle />
-						<TextField label="아이디" />
-					</Grid>
-					<Grid container className="login-field">
-						<LockIcon />
-						<TextField label="비밀번호" />
-					</Grid>
-					<Grid>
-						<Link to="/">
-							<Button
-								variant="outlined"
-								color="primary"
-								className="login-btn"
-							>
-								로그인
-							</Button>
-						</Link>
-					</Grid>
-				</Grid>
-			</Box>
-		</Grid>
-	);
+	return <div>로그인페이지</div>;
 };
 
 export default LoginView;
--- frontend/src/components/View/MainView.js
+++ frontend/src/components/View/MainView.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 import { Route } from 'react-router-dom';
 import { Header } from '../UI';
-import { Home, User, Problem, Login } from '.';
+import { Home, User, Problem } from '.';
 
 const MainView = () => {
 	const userName = '사용자';
@@ -13,8 +13,12 @@ const MainView = () => {
 			<Grid>
 				<Route exact path="/" component={Home} />
 				<Route exact path="/user" component={User} />
+<<<<<<< HEAD
 				<Route exact path="/problem" component={Problem} />
 				<Route exact path="/login" component={Login} />
+=======
+				<Route path="/problem" component={Problem} />
+>>>>>>> parent of f62b0bc... Fix conflicts
 			</Grid>
 		</>
 	);

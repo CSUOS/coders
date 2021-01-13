cc77c4bc340c036b9ae64ee430d74e19448b8ac7
leegwae <leegwae@gmail.com>
2021-01-14 03:28:50 +0900

    Update migrate.go

--- server/model/migrate.go
+++ server/model/migrate.go
@@ -1,13 +1,13 @@
 package model
 
-import (	
+import (
 	"fmt"
-	
+
 	"gorm.io/gorm"
 )
 
 // Migrate automigrates model using GORM
-func Migrate(db *gorm.DB){
-	db.AutoMigrate(&Ping{}, &Member{}, &Problem{})
+func Migrate(db *gorm.DB) {
+	db.AutoMigrate(&Ping{}, &Member{}, &Problem{}, &PComment{})
 	fmt.Println("--> Automigration finished")
 }

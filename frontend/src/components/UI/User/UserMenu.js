9e96fb13131178d3a598528fcbfc44dc791d3006
pySoo <55135881+pySoo@users.noreply.github.com>
2021-01-30 20:45:54 +0900

    검색 기능 추가

--- frontend/src/components/UI/User/UserMenu.js
+++ frontend/src/components/UI/User/UserMenu.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { Grid, MenuList, MenuItem, ListItemIcon } from '@material-ui/core';
+import {
+	Grid,
+	Paper,
+	MenuList,
+	MenuItem,
+	ListItemIcon,
+} from '@material-ui/core';
 import DoneOutlineIcon from '@material-ui/icons/DoneOutline';
 import CreateIcon from '@material-ui/icons/Create';
 import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
@@ -7,34 +13,34 @@ import StarIcon from '@material-ui/icons/Star';
 
 const UserMenu = () => {
 	return (
-		<Grid className="user-menu">
-			<MenuList>
-				<MenuItem>
-					<ListItemIcon>
-						<StarIcon fontSize="small" />
+		<Paper className="user-menu">
+			<MenuList className="menu-list">
+				<MenuItem className="menu-item">
+					<ListItemIcon className="menu-icon">
+						<StarIcon fontSize="big" />
 					</ListItemIcon>
 					즐겨찾기
 				</MenuItem>
-				<MenuItem>
-					<ListItemIcon>
-						<DoneOutlineIcon fontSize="small" />
+				<MenuItem className="menu-item">
+					<ListItemIcon className="menu-icon">
+						<DoneOutlineIcon fontSize="big" />
 					</ListItemIcon>
 					제출 문제
 				</MenuItem>
-				<MenuItem>
-					<ListItemIcon>
-						<QuestionAnswerIcon fontSize="small" />
+				<MenuItem className="menu-item">
+					<ListItemIcon className="menu-icon">
+						<QuestionAnswerIcon fontSize="big" />
 					</ListItemIcon>
 					작성 질문
 				</MenuItem>
-				<MenuItem>
-					<ListItemIcon>
-						<CreateIcon fontSize="small" />
+				<MenuItem className="menu-item">
+					<ListItemIcon className="menu-icon">
+						<CreateIcon fontSize="big" />
 					</ListItemIcon>
 					출제 문제
 				</MenuItem>
 			</MenuList>
-		</Grid>
+		</Paper>
 	);
 };
 

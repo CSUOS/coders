3fa2d621e1b06b69b80a332a744f3e25e7031aaa
pache <pachepot>
2021-02-01 15:08:40 +0900

    Add problem API

--- server/model/problem.go
+++ server/model/problem.go
@@ -6,38 +6,38 @@ import (
 )
 
 type Problem struct {
-	ID  		 int          `json:"id" example:"1" format:"int64" gorm:"autoIncrement"`
-	Title 		 string 	  `json:"title" example:"Problem title"`
-	Class		 string  	  `json:"class" example:"Problem class"`
-	Desc 		 string 	  `json:"desc" example:"Problem description"`
-	TimeLimit    int		  `json:"timeLimit" example:"1000" format:"int64"`
-	MemoryLimit  int 		  `json:"memoryLimit" example:"128" format:"int64"`
-	ShortCircuit bool 		  `json:"shortCircuit" example:"false"`
-	MemberID     int 		  `json:"memberID" example:"1" format:"int64"`
-	//Member 		 Member 	  `gorm:"ForeignKey:MemberID;"`
+	ID           int          `json:"id" example:"1" format:"int64" gorm:"autoIncrement"`
+	Title        string       `json:"title" example:"Problem title"`
+	Class        string       `json:"class" example:"Problem class"`
+	Desc         string       `json:"desc" example:"Problem description"`
+	TimeLimit    int          `json:"timeLimit" example:"1000" format:"int64"`
+	MemoryLimit  int          `json:"memoryLimit" example:"128" format:"int64"`
+	ShortCircuit bool         `json:"shortCircuit" example:"false"`
+	MemberID     int          `json:"memberID" example:"1" format:"int64"`
+	Member       Member       `gorm:"ForeignKey:MemberID;"`
 	Submissions  []Submission `gorm:"ForeignKey:ProblemID";constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 }
 
 type EditProblem struct {
-	Title		 string `json:"title" example:"Problem title"`
-	Class		 string `json:"class" example:"Problem class"`
-	Desc 		 string `json:"desc" example:"Problem description"`
-	TimeLimit    int 	`json:"timeLimit" example:"1000" format:"int64"`
-	MemoryLimit  int 	`json:"memoryLimit" example:"1024" format:"int64"`
+	Title        string `json:"title" example:"Problem title"`
+	Class        string `json:"class" example:"Problem class"`
+	Desc         string `json:"desc" example:"Problem description"`
+	TimeLimit    int    `json:"timeLimit" example:"1000" format:"int64"`
+	MemoryLimit  int    `json:"memoryLimit" example:"1024" format:"int64"`
 	ShortCircuit bool   `json:"shortCircuit" example:"false"`
 	MemberID     int    `json:"memberID" example:"1" format:"int64"`
 }
 
 type PrintProblem struct {
-	Title 		 string
-	Class        string
+	Title string
+	Class string
 }
 
 var (
-	ErrTitleInvalid = errors.New("title is empty")
-	ErrDescInvalid = errors.New("description is empty")
-	ErrMemberIDInvalid = errors.New("member ID is empty")
-	ErrTimeLimitInvalid = errors.New("time limit is empty")
+	ErrTitleInvalid       = errors.New("title is empty")
+	ErrDescInvalid        = errors.New("description is empty")
+	ErrMemberIDInvalid    = errors.New("member ID is empty")
+	ErrTimeLimitInvalid   = errors.New("time limit is empty")
 	ErrMemoryLimitInvalid = errors.New("memory limit is empty")
 )
 
@@ -58,26 +58,26 @@ func (p EditProblem) ProblemValidation() error {
 	}
 }
 
-func ProblemAll(db *gorm.DB, num int, page int, mid int, search string, sort string) ([]PrintProblem, error)  {
+func ProblemAll(db *gorm.DB, num int, page int, mid int, search string, sort string) ([]PrintProblem, error) {
 	var problem []PrintProblem
 	var err error
 
 	switch { // mid, search, sort의 공백 여부에 따라 분리
-	case mid==0 && search=="" && sort=="":
+	case mid == 0 && search == "" && sort == "":
 		err = db.Model(&Problem{}).Limit(num).Offset(num*(page-1)).Select("title", "class").Find(&problem).Error
-	case mid==0 && search=="" && sort!="":
+	case mid == 0 && search == "" && sort != "":
 		err = db.Model(&Problem{}).Order(sort).Limit(num).Offset(num*(page-1)).Select("title", "class").Find(&problem).Error
-	case mid==0 && search!="" && sort=="":
+	case mid == 0 && search != "" && sort == "":
 		err = db.Model(&Problem{}).Limit(num).Offset(num*(page-1)).Where("title LIKE ? OR class LIKE ?", "%"+search+"%", "%"+search+"%").Select("title", "class").Find(&problem).Error
-	case mid==0 && search!="" && sort!="":
+	case mid == 0 && search != "" && sort != "":
 		err = db.Model(&Problem{}).Order(sort).Limit(num).Offset(num*(page-1)).Where("title LIKE ? OR class LIKE ?", "%"+search+"%", "%"+search+"%").Select("title", "class").Find(&problem).Error
-	case mid!=0 && search=="" && sort=="":
+	case mid != 0 && search == "" && sort == "":
 		err = db.Model(&Problem{}).Limit(num).Offset(num*(page-1)).Where("member_id = ?", mid).Select("title", "class").Find(&problem).Error
-	case mid!=0 && search=="" && sort!="":
+	case mid != 0 && search == "" && sort != "":
 		err = db.Model(&Problem{}).Order(sort).Limit(num).Offset(num*(page-1)).Where("member_id = ?", mid).Select("title", "class").Find(&problem).Error
-	case mid!=0 && search!="" && sort=="":
+	case mid != 0 && search != "" && sort == "":
 		err = db.Model(&Problem{}).Limit(num).Offset(num*(page-1)).Where("member_id = ? AND (title LIKE ? OR class LIKE ?) ", mid, "%"+search+"%", "%"+search+"%").Select("title", "class").Find(&problem).Error
-	case mid!=0 && search!="" && sort!="":
+	case mid != 0 && search != "" && sort != "":
 		err = db.Model(&Problem{}).Order(sort).Limit(num).Offset(num*(page-1)).Where("member_id = ? AND (title LIKE ? OR class LIKE ?) ", mid, "%"+search+"%", "%"+search+"%").Select("title", "class").Find(&problem).Error
 	}
 
@@ -97,24 +97,36 @@ func ProblemInsert(db *gorm.DB, problem Problem) (Problem, error) {
 
 func ProblemUpdate(db *gorm.DB, problem Problem) (Problem, error) {
 	err := db.Model(&Problem{}).Where("id = ?", problem.ID).Update("title", problem.Title).Error
-	if err!=nil { return problem, err }
+	if err != nil {
+		return problem, err
+	}
 	err = db.Model(&Problem{}).Where("id = ?", problem.ID).Update("class", problem.Class).Error
-	if err!=nil { return problem, err }
+	if err != nil {
+		return problem, err
+	}
 	err = db.Model(&Problem{}).Where("id = ?", problem.ID).Update("desc", problem.Desc).Error
-	if err!=nil { return problem, err }
+	if err != nil {
+		return problem, err
+	}
 	err = db.Model(&Problem{}).Where("id = ?", problem.ID).Update("time_limit", problem.TimeLimit).Error
-	if err!=nil { return problem, err }
+	if err != nil {
+		return problem, err
+	}
 	err = db.Model(&Problem{}).Where("id = ?", problem.ID).Update("memory_limit", problem.MemoryLimit).Error
-	if err!=nil { return problem, err }
+	if err != nil {
+		return problem, err
+	}
 	err = db.Model(&Problem{}).Where("id = ?", problem.ID).Update("short_circuit", problem.ShortCircuit).Error
-	if err!=nil { return problem, err }
+	if err != nil {
+		return problem, err
+	}
 	return problem, err
 }
 
 func ProblemDelete(db *gorm.DB, id int) error {
 	var problem Problem
 	err := db.Where("id=?", id).First(&problem).Error
-	if err != nil{
+	if err != nil {
 		return err
 	}
 	err = db.Delete(&problem).Error
